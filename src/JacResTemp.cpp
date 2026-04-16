68d1f2dcc19cfab03bc991be6f31d0994bfe83db
Panupong Kongpet <pkongpet@hawaii.edu>
2026-04-15 17:56:39 -1000

    fix km to Km and kp to Kp

--- src/JacResTemp.cpp
+++ src/JacResTemp.cpp
@@ -528,7 +528,7 @@ PetscErrorCode JacResGetTempRes(JacRes *jr, PetscScalar dt)
 		{
 			bdpdx = ((Pc - P[k][j][Im1])/bdx)*vx[k][j][i];        fdpdx = ((P[k][j][Ip1] - Pc)/fdx)*vx[k][j][i+1];
 			bdpdy = ((Pc - P[k][Jm1][i])/bdy)*vy[k][j][i];        fdpdy = ((P[k][Jp1][i] - Pc)/fdy)*vy[k][j+1][i];
- 			bdpdz = ((Pc - P[km1][j][i])/bdz)*vz[k][j][i];   fdpdz = ((P[kp1][j][i] - Pc)/fdz)*vz[k+1][j][i];
+ 			bdpdz = ((Pc - P[Km1][j][i])/bdz)*vz[k][j][i];   fdpdz = ((P[Kp1][j][i] - Pc)/fdz)*vz[k+1][j][i];
             //if on bottom boundary use the same dpdz as above the cell with the correct vz.
             if (k==0) bdpdz = ((P[k+1][j][i] - Pc)/fdz)*vz[k][j][i];  
 			// Adiabatic Heat term
